9e0fddc49b2b24575811337c487be0b885de8a7c
Elliot Kirk <elliot.s.kirk@gmail.com>
2017-11-06 11:41:14 -0800

    Screen interface

---
+++ src/Screen.java
@@ -0,0 +1,10 @@
+import javafx.scene.*;
+import javafx.scene.layout.GridPane;
+
+public interface Screen {
+	
+	public Scene getScene();
+		//	GridPane gridpane;
+		//	Scene scene = new Scene(gridpane, 800, 600);
+		// 	return scene
+}
