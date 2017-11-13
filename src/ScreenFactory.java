56e3d4b6d1e1f2b37f4897bdc37083da7e386d4c
rpunao <32205708+rpunao@users.noreply.github.com>
2017-11-12 17:37:04 -0800

    Changed commented section to have correct if statements

--- src/ScreenFactory.java
+++ src/ScreenFactory.java
@@ -2,30 +2,24 @@ import javafx.scene.Scene;
 
 public class ScreenFactory extends Main{
 	
-	//screenTypes: "Main Menu" | "Login" | "Settings" | etc
 	public void newScreen(String screenType)
 	{
 		Screen screen = null;
 		Scene scene = null;
 		if(screenType.equals("Main Menu"))
-		{
 			screen = new MainMenu();
-		}
-		
-		else// if(screenType.equals("Login"))
+		else if(screenType.equals("Login"))
 			screen = new Login();
 	/*	else if(screenType.equals("Play Online"))
-			scene = PlayOnline.getScene();
+			screen = new PlayOnline();
 		else if(screenType.equals("Play Offline"))
-			scene = PlayOffline.getScene();
+			screen = new PlayOffline();
 		else if(screenType.equals("Settings"))
-			scene = Settings.getScene();
+			screen = new Settings();
 		else if(screenType.equals("Leaderboard"))
-			scene = Leaderboard.getScene();
+			screen = new Leaderboard();
 		else if(screenType.equals("Tutorial"))
-			scene = PlayOnline.getScene();
-		else if(screenType.equals("Play Online"))
-			scene = PlayOnline.getScene();
+			screen = new Tutorial();
 	*/	
 		scene = screen.getScene();
 		updateScene(scene);
