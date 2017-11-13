3f83a97af6e3764ff9859f58b422bfc344439148
Ryan Lawrence <rtlawren@calpoly.edu>
2017-11-12 16:25:29 -0800

    Added a settings screen and changed the Screen interface.

--- src/Screen.java
+++ src/Screen.java
@@ -1,10 +1,6 @@
 import javafx.scene.*;
-import javafx.scene.layout.GridPane;
 
 public interface Screen {
 	
 	public Scene getScene();
-		//	GridPane gridpane;
-		//	Scene scene = new Scene(gridpane, 800, 600);
-		// 	return scene
 }
