1f0077d2a95b122ad8618f88c83117483f60140b
Mark Toujiline <mark.toujiline@gmail.com>
2017-11-17 11:39:45 -0800

    44 Game Loop Added

--- src/game/GameEngine.java
+++ src/game/GameEngine.java
@@ -1,16 +1,76 @@
 package game;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
 
 //remove class after scene is complete
-public class GameEngine extends Application{
-	@Override
-    public void start(final Stage primaryStage) {
+public class GameEngine implements Runnable {
+	
+	private Thread thread;
+	private boolean running = false;
+	private final double UPDATE_CAP = 1.0/60.0;
+	
+	public GameEngine() {
+		
+	}
+	public void start() {
+		thread = new Thread(this);
+		thread.run();
+	}
+	public void stop() {
 		
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
+	public void run() {
+		running = true;
+		
+		boolean render = false;
+		
+		double firstTime = 0;
+		double lastTime = System.nanoTime() / 1000000000.0;
+		double passedTime = 0;
+		double unprocessedTime = 0;
+		
+		double frameTime = 0;
+		int frames = 0;
+		int fps = 0;
+		
+		while(running) {
+			
+			render = false;
+			firstTime = System.nanoTime() / 1000000000.0;
+			passedTime = firstTime - lastTime;
+			lastTime = firstTime;
+			
+			unprocessedTime += passedTime;
+			frameTime += passedTime;
+			
+			while(unprocessedTime >= UPDATE_CAP) {
+				unprocessedTime -= UPDATE_CAP;
+				render = true;
+				//TODO: Update game
+				if (frameTime >= 1.0) {
+					frameTime = 0;
+					fps = frames;
+					frames = 0;
+					System.out.println("FPS: " + fps);
+				}
+			}
+			
+			if (render) {
+				//TODO: Render game
+				frames++;
+			}
+			else {
+				try {
+					Thread.sleep(1);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		System.out.println("Game is done");
+	}
+	private void dispose() {
+		
 	}
 }
