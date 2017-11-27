63be2936548146954e3739f73ddd94f7d56ebe22
Ryan Lawrence <rtlawren@calpoly.edu>
2017-11-27 11:55:59 -0800

    Created a test suite to more easily run all unit tests.

---
+++ src/test/AllUnitTests.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AllBallTests.class, AllBasicScreenTests.class, AllGameplayScreenTests.class, AllGameTests.class,
+		AllHelperClassTests.class, AllUserProfileTests.class })
+public class AllUnitTests {
+
+}
